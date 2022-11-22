bda4e3fb4827e43c8a5c2de60fd72c93d0e7fb36
Hotcer <118493380+Hotcer@users.noreply.github.com>
2022-11-22 11:49:07 -0500

    Resolucio ejercicio crea un array unidimensional de Strings y recórrelo, mostrando únicamente sus valores.

---
+++ Arrays/ArrayUnidimensional.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArrayUnidimensional {
+    public static void main(String[] args) {
+        String[] distrosLinux = {"Ubuntu", "Linux Mint","Manjaro","Debian", "Fedora" };
+        for (String paco: distrosLinux) {
+            System.out.println(paco);
+        }
+
+    }
+}
