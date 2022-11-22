1a2a0cf91b508324cb2d1ab5642bd6876d4de91a
Hotcer <118493380+Hotcer@users.noreply.github.com>
2022-11-22 17:21:51 -0500

    Resolucion crea un array bidimensional de enteros y recórrelo, mostrando la posición y el valor de cada elemento en ambas dimensiones.

---
+++ Arrays/ArrayBidimensional.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ArrayBidimensional {
+    public static void main(String[] args) {
+        int [] [] arreglo={
+                {1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}
+
+        } ;
+        for (int i = 0; i <arreglo.length ; i++) {
+            for (int j = 0; j < arreglo[0].length; j++) {
+                System.out.println(arreglo[i] [j]);
+            }
+        }
+
+
+
+        }
+    }
+
